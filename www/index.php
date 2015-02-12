beef1d52581b986aaf0bf254147ac426a61520cb
Mbrune <cmmg24@mst.edu>
2015-02-12 14:02:09 -0600

    Uploaded Website
    Look to title

---
+++ www/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Onyx Electronyx</title>
+</head>
+<html>
+<head>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type"></meta>
+	<title>Mbrune</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+	<body id="homepage">
+		<div class="menu-page">
+			<div id="topbar">
+				<div class="wrapper">
+					<div class="column one-one" style="">
+						<form id="searchform" action="http://www.onyxelectronics.com/searchresults" method="post">
+							<div class="hiddenFields">
+								<input type="hidden" value="2" name="ACT"></input>
+								<input type="hidden" value="" name="RES"></input>
+								<input type="hidden" value="qI5T5Rf/jojGdtwMthfPIiIRD7Bem1JDqeOpz6oPG2pHUi5nt4dt2qoK1UrV…pnOksMry0U3U8riC422i8Odxqkmergj3kYk8XJXqLjEAhyYknj32Q7DeaAg=" name="meta"></input
+								<input type="hidden" value="1" name="site_id"></input>
+								<input type="hidden" value="8486b3a0fdeb34b1c87a7e1fef4df140f8f59ef6" name="csrf_token"></input>
+							</div>
+								<input class="btn" type="submit" value="Search"></input>
+								<input id="keywords" class="field" type="text" maxlength="100" size="18" onblur="if(this.value == ''){ this.value = 'Search Onyx.com'; }" onfocus="if(this.value == 'Search Onyx.com'){ this.value = ''; }" value="Search Onyx.com" name="Search Onyx.com"></input>
+						</form>
+							<ul>
+							<li>
+								<a target="_blank" title="Mbrune website" href="index.html">Onyx Homepage</a>
+							</li>
+							<li>
+								<a target="_blank" title="Mbrune website" href="forums.html">Onyx Forums</a>
+							</li>
+							<li>
+								<a target="_blank" title="Mbrune's Second Website" href="shop.html">Onyx Shop</a>
+							</li>
+							<li>
+								<a target="_blank" title="Login" href="login.html"> Login</a>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="header">
+				<div class="wrapper">
+					<div class="column one-one" style="">
+						<a id="logo" title="Mbrune" href="/">Onyx</a>
+						<ul id="nav">
+							<li>
+								<a id="home" title="Home" href="/">
+									Home
+								</a>
+							</li>
+							<li>
+								<a id="projects" title="Projects" href="projects.html">Projects</a>
+							</li>
+								<ul style="overflow: hidden; display: none; height 135px; padding-top:...x; margin-top: 0px; padding-bottom: 4px; margin-bottom: 0px;">
+									<li>
+										<a href="/projects/webdevelopment"></a>Web Development</li>
+									<li>
+										<a href="/projects/aeronotics"></a>Aeronotics</li>
+									<li>
+										<a href="/projects/webdevelopment"></a>Web D</li>
+									<li>
+										<a href="/projects/webdevelopment"></a>Aeronotics</li>									
+								</ul>
+							<li>
+								<a id="software" title="Software" href="/">Software</a>
+							</li>
+							<li>
+								<a id="research" title="Research" href="/">Research</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="content">
+				<div class="wrapper">
+					<ul>
+
+
+						<h2>Dying Light looks pretty flligh</h2>
+
+						<li><img class="quarter" src="assets/media/images/DyingLight1.png"/><img class="quarter" src="assets/media/images/DyingLight2.png"/></li>
+						
+						<li><video class="full" controls>
+ 						<source src="assets/media//videos/DyingLightTrailer.mp4" type="video/mp4"></video></li>
+
+						<h2>oh snap! look at dat HoloLens</h2>
+
+						<li><img class="full" src="assets/media/images/HoloLens1.png">						
+						<h2>The Crew</h2>
+
+						<li><video class="full" controls>
+ 						<source src="assets/media//videos/TheCrew.mp4" type="video/mp4"></video></li></li>
+					</ul>
+				</div>
+			</div>
+			<div id="social">
+				<div class="wrapper">
+					<ul>
+						<li id="facebook">
+							<a href="https://www.facebook.com/pages/Onyx-Electronyx/597548963711400"><img src="assets/media/images/FacebookDark.png" onmouseover="this.src='assets/media/images/Facebook.png'" onmouseout="this.src='assets/media/images/FacebookDark.png'" /></a>
+						</li>
+						<li id="googleplus">
+							<a href="https://plus.google.com/u/0/100354697607903274698/posts"><img src="assets/media/images/Google+Dark.png" onmouseover="this.src='assets/media/images/Google+.png'" onmouseout="this.src='assets/media/images/Google+Dark.png'" /></a>
+						</li>
+						<li id="tumblr">
+							<a href="https://www.tumblr.com/blog/onyxelectronyx"><img src="assets/media/images/TumblrDark.png" onmouseover="this.src='assets/media/images/Tumblr.png'" onmouseout="this.src='assets/media/images/TumblrDark.png'" /></a>
+						</li>
+						<li id="twitter">
+							<a href="https://twitter.com/OnyxElectronyx"><img src="assets/media/images/TwitterDark.png" onmouseover="this.src='assets/media/images/Twitter.png'" onmouseout="this.src='assets/media/images/TwitterDark.png'" /></a>
+						</li>
+						<li id="youtube">
+							<a href="https://www.youtube.com/channel/UCtWZ6Sz-eM9TlVu3gHS_VNA"><img src="assets/media/images/YoutubeDark.png" onmouseover="this.src='assets/media/images/Youtube.png'" onmouseout="this.src='assets/media/images/YoutubeDark.png'" /></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div id="footer">
+				<div class="wrapper">
+					<div class="row" style="">
+						<h5>
+							<a title="Information" href="www.onyxelectronics.com/Information/"></a>Information</h5>
+						<ul>
+							<li>
+								<a href="www.onyxelectronyx.com/website/">Websites</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/technology/">Technology</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/research/">Research</a>
+							</li>
+						</ul>
+					</div>
+					<div class="row" style="">
+						<h5>
+							<a title="Donate" href="www.onyxelectronyx.com/donate"></a>Donate</h5>
+						<ul>
+							<li>
+								<a href="www.onyxelectronyx.com/website/">Websites</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/technology/">Technology</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/research/">Research</a>
+							</li>
+						</ul>
+					</div>
+					<div class="row" style="">
+						<h5>
+							<a title="Careers" href="www.onyxelectronyx.com/careers"></a>Careers</h5>
+						<ul>
+							<li>
+								<a href="www.onyxelectronyx.com/website/">Computer Science</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/technology/">Aviation</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/research/">Management</a>
+							</li>
+						</ul>
+					</div>
+					<div class="row" style="">
+						<h5>
+							<a title="" href="www.onyxelectronyx.com/careers"></a>Careers</h5>
+						<ul>
+							<li>
+								<a href="www.onyxelectronyx.com/website/">Computer Science</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/technology/">Aviation</a>
+							</li>
+							<li>
+								<a href="www.onyxelectronyx.com/research/">Management</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
